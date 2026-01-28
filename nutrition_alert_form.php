8b0631d62f1feca52d9a9e80a30b3cbd6571cfa5
pchoeiphet <phetrada.2646@gmail.com>
2026-01-28 14:24:18 +0700

    up

--- nutrition_alert_form.php
+++ nutrition_alert_form.php
@@ -1014,6 +1014,9 @@ try {
         </form>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
     <script>
         // ฟังก์ชันเปิด/ปิด ช่องระบุอื่นๆ
         function toggleOtherSource() {
